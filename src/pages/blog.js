99feece40db9b9dffd698ac97ba588e63ae5309a
Bernardo <moonbe77@gmail.com>
2021-07-13 18:15:18 +1000

    add filter to query ===  only blog folder

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,16 +5,14 @@ import Layout from '../components/Layout'
 
 const BlogPage = ({ data }) => {
 
-  // console.log(data);
+  console.log(data);
 
   return (
     <Layout pageTitle="My Blog Posts">
       <p>My cool posts will go in here</p>
       <ul>
         {data.allFile.nodes.map(file => {
-          if (file.sourceInstanceName === 'blog') {
-            return (<li key={file.id}>{file.name}</li>)
-          }
+          return (<li key={file.id}>{file.name}</li>)
         })}
       </ul>
     </Layout>
@@ -23,15 +21,12 @@ const BlogPage = ({ data }) => {
 
 
 
-export const query = graphql`
-query {
-  allFile{
+export const query = graphql`  {
+  allFile(filter: {sourceInstanceName: {eq: "blog"}}) {
     nodes {
-      name,
-      id,
-      ext,
-      base,
-      sourceInstanceName,
+      name
+      ext
+      id
     }
   }
 }
